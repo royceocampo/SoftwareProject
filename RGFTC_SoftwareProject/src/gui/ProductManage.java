006b605bb351207f2fe4189e24dc78a76d1f2915
royceocampo <royceocampo@yahoo.com>
2016-02-24 22:13:55 +0800

    Revert "ProductManagement"
    This reverts commit 51ad2a95fa2be73a6c604727141d753774fdab2c.

--- RGFTC_SoftwareProject/src/gui/ProductManage.java
+++ RGFTC_SoftwareProject/src/gui/ProductManage.java
@@ -3,24 +3,10 @@ package gui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.custom.TableTree;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.graphics.Point;
 
 public class ProductManage {
 
-	protected Shell shlRareGlobalFoods;
-	private Text txtSearch;
+	protected Shell shell;
 
 	/**
 	 * Launch the application.
@@ -41,9 +27,9 @@ public class ProductManage {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shlRareGlobalFoods.open();
-		shlRareGlobalFoods.layout();
-		while (!shlRareGlobalFoods.isDisposed()) {
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -54,106 +40,10 @@ public class ProductManage {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shlRareGlobalFoods = new Shell();
-		shlRareGlobalFoods.setMinimumSize(new Point(500, 200));
-		shlRareGlobalFoods.setBackground(SWTResourceManager.getColor(SWT.COLOR_INFO_BACKGROUND));
-		shlRareGlobalFoods.setSize(450, 324);
-		shlRareGlobalFoods.setText("Rare Global Food Trading Corp.");
-		
-		Menu menu = new Menu(shlRareGlobalFoods, SWT.BAR);
-		shlRareGlobalFoods.setMenuBar(menu);
-		
-		MenuItem mntmHome = new MenuItem(menu, SWT.NONE);
-		mntmHome.setText("Home");
-		
-		MenuItem mntmOrder = new MenuItem(menu, SWT.NONE);
-		mntmOrder.setText("Order");
-		
-		MenuItem mntmProduct = new MenuItem(menu, SWT.NONE);
-		mntmProduct.setText("Product");
-		
-		MenuItem mntmInventory = new MenuItem(menu, SWT.NONE);
-		mntmInventory.setText("Inventory");
-		
-		Label lblProductInventory = new Label(shlRareGlobalFoods, SWT.NONE);
-		lblProductInventory.setBackground(SWTResourceManager.getColor(SWT.COLOR_INFO_BACKGROUND));
-		lblProductInventory.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_RED));
-		lblProductInventory.setFont(SWTResourceManager.getFont("Segoe UI", 18, SWT.NORMAL));
-		lblProductInventory.setBounds(10, 10, 249, 38);
-		lblProductInventory.setText("Product Management");
-		
-		Label lblViewBy = new Label(shlRareGlobalFoods, SWT.NONE);
-		lblViewBy.setBackground(SWTResourceManager.getColor(SWT.COLOR_INFO_BACKGROUND));
-		lblViewBy.setBounds(10, 54, 55, 15);
-		lblViewBy.setText("View By:");
-		
-		CCombo combo = new CCombo(shlRareGlobalFoods, SWT.BORDER);
-		combo.setBounds(71, 54, 106, 21);
-		
-		CCombo combo_1 = new CCombo(shlRareGlobalFoods, SWT.BORDER);
-		combo_1.setBounds(183, 54, 106, 21);
-		
-		TableTree tableTree = new TableTree(shlRareGlobalFoods, SWT.BORDER | SWT.FULL_SELECTION);
-		tableTree.setBounds(27, 87, 397, 117);
-		
-		TableColumn tblclmnProduct = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnProduct.setWidth(100);
-		tblclmnProduct.setText("Product");
-		
-		TableColumn tblclmnCategory = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnCategory.setWidth(100);
-		tblclmnCategory.setText("Category");
-		
-		TableColumn tblclmnSubtype = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnSubtype.setWidth(100);
-		tblclmnSubtype.setText("SubType");
-		
-		TableColumn tblclmnBrand = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnBrand.setWidth(100);
-		tblclmnBrand.setText("Brand");
-		
-		TableColumn tblclmnPackaging = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnPackaging.setWidth(100);
-		tblclmnPackaging.setText("Packaging");
-		
-		TableColumn tblclmnPrice = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnPrice.setWidth(100);
-		tblclmnPrice.setText("Price");
-		
-		TableColumn tblclmnBox = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnBox.setWidth(100);
-		tblclmnBox.setText("Box");
-		
-		TableColumn tblclmnWeight = new TableColumn(tableTree.getTable(), SWT.NONE);
-		tblclmnWeight.setWidth(100);
-		tblclmnWeight.setText("Weight");
-		
-		Button btnNewButton = new Button(shlRareGlobalFoods, SWT.NONE);
-		btnNewButton.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
-		btnNewButton.setForeground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION_TEXT));
-		btnNewButton.setBounds(27, 223, 95, 21);
-		btnNewButton.setText("Add Product");
-		
-		Button btnEdit = new Button(shlRareGlobalFoods, SWT.NONE);
-		btnEdit.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		btnEdit.setText("Edit");
-		btnEdit.setForeground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION_TEXT));
-		btnEdit.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
-		btnEdit.setBounds(143, 223, 95, 21);
-		
-		Button btnDelete = new Button(shlRareGlobalFoods, SWT.NONE);
-		btnDelete.setText("Delete");
-		btnDelete.setForeground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION_TEXT));
-		btnDelete.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
-		btnDelete.setBounds(255, 223, 95, 21);
-		
-		txtSearch = new Text(shlRareGlobalFoods, SWT.BORDER);
-		txtSearch.setText("Search");
-		txtSearch.setBounds(297, 54, 127, 21);
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
 
 	}
+
 }
