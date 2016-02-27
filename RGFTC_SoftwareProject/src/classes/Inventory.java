a384e2ed7842dc968746fd3988f803af6d5223c3
Rodikuhlus <Rodikuhlus@users.noreply.github.com>
2016-02-27 13:32:26 +0800

    Updated it
    This was based on the Diagrams we submitted before

--- RGFTC_SoftwareProject/src/classes/Inventory.java
+++ RGFTC_SoftwareProject/src/classes/Inventory.java
@@ -2,4 +2,60 @@ package classes;
 
 public class Inventory {
 
+	private int inventoryID;
+	private Product product;
+	private int stockAmount;
+	private int boxAmount;
+	private float weight;
+	
+	public Inventory(){}
+	
+	public Inventory(Product product, int stockAmount, int boxAmount, float weight){
+		this.product = product;
+		this.stockAmount = stockAmount;
+		this.boxAmount = boxAmount;
+		this.weight = weight;
+	}
+
+	public int getInventoryID() {
+		return inventoryID;
+	}
+
+	public void setInventoryID(int inventoryID) {
+		this.inventoryID = inventoryID;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+	public int getBoxAmount() {
+		return boxAmount;
+	}
+
+	public void setBoxAmount(int boxAmount) {
+		this.boxAmount = boxAmount;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	
+	
 }
