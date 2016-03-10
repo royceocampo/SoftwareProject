7807a6e1fab1da35c9f439123bcd682115d4c76d
royceocampo <royceocampo@yahoo.com>
2016-03-11 01:06:40 +0800

    Added DB file and Libraries needed
    SQLconnector, Swing2SWT, SWTjar ZIP file in Libraries folder contains:
    1. SQL Connector
    2. Swing2SWT
    3. SWT version (x86 and x64) that will be needed for the build path of
    the org.eclipse.swt package once it is imported in the workspace

--- RGFTC_SoftwareProject/sql/rgftc_db.sql
+++ RGFTC_SoftwareProject/sql/rgftc_db.sql
@@ -26,12 +26,12 @@ DROP TABLE IF EXISTS `inventory_table`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `inventory_table` (
   `inventoryID` int(11) NOT NULL AUTO_INCREMENT,
+  `productID` int(11) NOT NULL,
   `person` varchar(45) NOT NULL,
   `product` varchar(45) NOT NULL,
-  `deposits` int(11) NOT NULL,
-  `withdrawals` int(11) NOT NULL,
+  `depwith` varchar(1) NOT NULL,
+  `qty` int(6) NOT NULL,
   `date` date NOT NULL,
-  `productID` int(11) NOT NULL,
   PRIMARY KEY (`inventoryID`),
   KEY `productKey_idx` (`productID`),
   CONSTRAINT `productKey` FOREIGN KEY (`productID`) REFERENCES `products_table` (`productID`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -55,7 +55,7 @@ DROP TABLE IF EXISTS `lineitems_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lineitems_table` (
-  `lineID` int(11) NOT NULL,
+  `lineID` int(11) NOT NULL AUTO_INCREMENT,
   `purchase_orderID` int(11) NOT NULL,
   `productID` int(11) NOT NULL,
   `pricePerKilo` float NOT NULL,
@@ -65,7 +65,7 @@ CREATE TABLE `lineitems_table` (
   KEY `productKey_idx` (`productID`),
   CONSTRAINT `orderKey` FOREIGN KEY (`purchase_orderID`) REFERENCES `orders_table` (`purchase_orderID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `productsKey` FOREIGN KEY (`productID`) REFERENCES `products_table` (`productID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,6 +74,7 @@ CREATE TABLE `lineitems_table` (
 
 LOCK TABLES `lineitems_table` WRITE;
 /*!40000 ALTER TABLE `lineitems_table` DISABLE KEYS */;
+INSERT INTO `lineitems_table` VALUES (1,1,1,430,2),(2,2,2,650,1),(3,3,3,110,1),(4,4,1,430,2),(5,5,1,430,2),(6,6,3,110,2),(7,7,2,430,1),(34,31,1,1,1);
 /*!40000 ALTER TABLE `lineitems_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,10 +91,10 @@ CREATE TABLE `orders_table` (
   `price` float NOT NULL,
   `orderReceiver` varchar(45) NOT NULL,
   `deliveryDueDate` date NOT NULL,
-  `deliveryStatus` tinyint(1) NOT NULL,
   `notes` varchar(45) NOT NULL,
+  `deliveryStatus` tinyint(1) NOT NULL,
   PRIMARY KEY (`purchase_orderID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +103,7 @@ CREATE TABLE `orders_table` (
 
 LOCK TABLES `orders_table` WRITE;
 /*!40000 ALTER TABLE `orders_table` DISABLE KEYS */;
-INSERT INTO `orders_table` VALUES (1,'Rod',12,'Eya','2016-12-12',0,'wala');
+INSERT INTO `orders_table` VALUES (1,'Waye',10,'Rod','2016-03-04','1',1),(2,'Waye',20,'Rod','2016-03-04','2',1),(3,'Waye',30,'Rod','2016-03-05','3',1),(4,'Thomas',40,'JP','2016-03-04','4',1),(5,'Thomas',50,'Rod','2016-03-05','5',1),(6,'Dane',60,'JP','2016-03-04','6',1),(7,'Dane',70,'Rod','2016-03-05','7',1),(8,'Dane',80,'Rod','2016-03-06','8',1),(31,'Waye',1,'HILAARY','2016-02-02','HELLO',0);
 /*!40000 ALTER TABLE `orders_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,13 +119,13 @@ CREATE TABLE `products_table` (
   `productName` varchar(45) NOT NULL,
   `category` varchar(45) NOT NULL,
   `subtype` varchar(45) NOT NULL,
-  `brand` varchar(45) DEFAULT NULL,
-  `packaging` varchar(45) DEFAULT NULL,
+  `brand` varchar(45) NOT NULL,
+  `packaging` varchar(45) NOT NULL,
   `pricePerKilo` float NOT NULL,
-  `stocks` int(11) unsigned zerofill DEFAULT NULL,
-  `weight` int(10) unsigned zerofill DEFAULT NULL,
+  `stocks` int(11) NOT NULL DEFAULT '0',
+  `weight` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
   PRIMARY KEY (`productID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +134,7 @@ CREATE TABLE `products_table` (
 
 LOCK TABLES `products_table` WRITE;
 /*!40000 ALTER TABLE `products_table` DISABLE KEYS */;
-INSERT INTO `products_table` VALUES (7,'Wagyu Rib Lifter Meat','Meat','Beef','SRF',NULL,900,NULL,NULL),(8,'Baby Back Ribs','Meat','Pork','Patel',NULL,420,NULL,NULL),(9,'Omi Beef','Meat','Japanese Wagyu',NULL,NULL,9500,NULL,NULL),(10,'Shabu-shabu','Meat','Cuts',NULL,NULL,7200,NULL,NULL),(11,'Black Tiger','Seafood','Shrimp',NULL,NULL,760,NULL,NULL),(12,'Abomasum','Meat','Beef','Swift',NULL,420,NULL,NULL),(13,'Tuna Sashimi','Seafood','Tuna',NULL,NULL,265,NULL,NULL),(14,'Baby Squid','Seafood','Squid/Octopus',NULL,NULL,160,NULL,NULL),(15,'Mussels (whole)','Seafood','Scallops & Shellfish',NULL,NULL,370,NULL,NULL),(16,'Gindara HGT','Seafood','Fish ',NULL,NULL,1650,NULL,NULL),(19,'Ground Beef 80/20','Meat','Beef','','',24,NULL,NULL),(20,'Vannamei','Seafood','Shrimps','','1kg/pack; 10kgs/box',65,NULL,NULL);
+INSERT INTO `products_table` VALUES (1,'Baby back ribs','Prok','Beef','Patel',' ',420,10,0000000000),(2,'Brisket','Beef',' Beef','Excel / US',' ',650,5,0000000000),(3,'Cream Dory Trimmed','Cream Dory','Beef',' ','10/box',110,2,0000000000),(5,'Beeeeef','Meat','Beef','BEF','BP',2,0,0000000000),(6,'RODIKHULUS','Meat','Cuts','SUPER CUTS','CUTLORD',999,0,0000000000),(7,'HElo','Meat','Beef','123`12','sdf',4,0,0000000000),(8,'1','Seafood','Nobashi','123','11',3,0,0000000000),(9,'SALMIN','Seafood','Fish (Whole, Fillet, Steak)','','',600,0,0000000000);
 /*!40000 ALTER TABLE `products_table` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -146,4 +147,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-04 18:57:59
+-- Dump completed on 2016-03-11  0:53:37
